7068707af728dd47743f82d59c1d6de1db49d49a
Robert Yassim Da Cunha Viera <robert.dacunhaviera@plexus.tech>
2024-06-25 12:54:37 +0200

    creacion del directorio dbz y sus componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 
 import { CounterModule } from './counter/counter.module';
 import { HeroModule } from './heroes/hero.module';
+import { DbzModule } from  './dbz/dbz.module'
+
 
 @NgModule({
   declarations: [
@@ -17,6 +19,9 @@ import { HeroModule } from './heroes/hero.module';
     AppRoutingModule,
     CounterModule,
     HeroModule,
+    DbzModule
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
